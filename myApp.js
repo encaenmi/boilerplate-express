718d75286cf11ba97f1a5f71ff213a0f1c274801
encaenmi <ece20@georgetown.edu>
2022-03-14 02:02:05 +0000

    3rd challenge

--- myApp.js
+++ myApp.js
@@ -3,10 +3,17 @@ var app = express();
 
 console.log("Hello World");
 
+/* first challenge
 app.get("/", function(req, res) {
   res.send("Hello Express");
 })
+*/
 
+// second challenge
+app.get("/", function(req, res) {
+	let fileUrl = __dirname + "/views/index.html";
+	res.sendFile(fileUrl);
+})
 
 
 
